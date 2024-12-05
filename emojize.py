e53754653179ffd9fa02c30076de747cdf45b1cb
Alex Gakuru <gakurualexgicimo@gmail.com>
2024-12-05 12:36:20 +0300

    emojize

--- emojize.py
+++ emojize.py
@@ -1,7 +1,10 @@
+import emoji
 class Emojize:
+    
     def main(self)-> None:
         user_st = input("Input: ")
+        print(f"Output: {emoji.emojize(user_st, language = 'alias')}")
 
 if __name__ == "__main__":
-    emojize = Emojize()
-    emojize.main()
+    emojize_ = Emojize()
+    emojize_.main()
